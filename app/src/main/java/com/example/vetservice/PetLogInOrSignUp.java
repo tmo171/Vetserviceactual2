bb46526db848f558dd02c8e839adbdee2b059274
toby <toby.morley171@gmail.com>
2020-10-07 17:54:29 +0100

    most recent

--- app/src/main/java/com/example/vetservice/PetLogInOrSignUp.java
+++ app/src/main/java/com/example/vetservice/PetLogInOrSignUp.java
@@ -7,7 +7,7 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 
-class PetLogInOrSignUp extends AppCompatActivity {
+public class PetLogInOrSignUp extends AppCompatActivity {
 
     @Override
 
